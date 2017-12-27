486c84e1737b98555872bef82f9ac09f925e3123
Pavel Chistyakov <p000000000001@gmail.com>
2017-12-27 22:26:55 +0200

    Проект парсера текста

--- io_text.cpp
+++ io_text.cpp
@@ -2,63 +2,38 @@
 
 using namespace io;
 
-text::text(stream& parent, codepages cp_src, codepages cp_dst) : parent(parent), cp_src(cp_src), cp_dst(cp_dst) {
+text::text(io::stream& parent, codepages cp) : cp(cp),
+parent(parent), cashed_pos(0), cashed_count(0) {
 }
 
-void text::autodetect(unsigned& result) {
-	if(cp_src != CPNONE || result != 0xEF)
-		return;
-	cp_src = CP1251;
-	if(get() != 0xBB)
-		return;
-	if(get() != 0xBF)
-		return;
-	cp_src = CPUTF8;
-	result = get();
+void text::shift() {
+	auto c = cashed_count - cashed_pos;
+	memcpy(cashed, cashed + cashed_pos, c);
+	cashed_count = c;
+	cashed_pos = 0;
 }
 
-unsigned char text::get() {
-	unsigned char sym;
-	if(!parent.read(&sym, sizeof(sym)))
-		return 0;
-	return sym;
-}
-
-int	text::seek(int count, int rel) {
-	return 0;
-}
-
-int	text::write(const void* result, int count) {
-	return 0;
-}
-
-int text::read(void* result, int count) {
-	auto pb = (char*)result;
-	auto pe = pb + count;
-	while(pb < pe) {
-		auto value = getu();
-		if(!value)
-			break;
-		switch(cp_dst) {
-		case CPUTF8:
-			if(((unsigned short)value) < 128) {
-				if(pb < pe)
-					*pb++ = (unsigned char)value;
-			} else if(((unsigned short)value) < 2047) {
-				if(pb < pe)
-					*pb++ = (unsigned char)(192 + (((unsigned short)value) / 64));
-				if(pb < pe)
-					*pb++ = (unsigned char)(128 + (((unsigned short)value) % 64));
-			} else {
-				if(pb < pe)
-					*pb++ = (unsigned char)(224 + (((unsigned short)value) / 4096));
-				if(pb < pe)
-					*pb++ = (unsigned char)(128 + ((((unsigned short)value) / 64) % 64));
-				if(pb < pe)
-					*pb++ = (unsigned char)(224 + (((unsigned short)value) % 64));
-			}
-			break;
-		case CP1251:
+bool text::makecashe(unsigned count) {
+	if(cashed_count - cashed_pos >= count)
+		return true;
+	if(cp == CPNONE) {
+		cashed_count += parent.read(cashed, 3);
+		if(cashed_count >= 3 && ((unsigned char)cashed[0]) == 0xEF && ((unsigned char)cashed[1]) == 0xBB && ((unsigned char)cashed[2]) == 0xBF) {
+			cp = CPUTF8;
+			cashed_pos += 3;
+			shift();
+			return makecashe(count);
+		} else
+			cp = CP1251;
+	}
+	if(cashed_pos)
+		shift();
+	auto size = sizeof(cashed) / sizeof(cashed[0]) - cashed_count;
+	if(cp == CP1251)
+		cashed_count += parent.read(cashed + cashed_count, size);
+	else {
+		while(size != 0) {
+			auto value = getu();
 			if(value >= 0x410 && value <= 0x44F)
 				value = value - 0x410 + 0xC0;
 			else switch(value) {
@@ -67,63 +42,141 @@ int text::read(void* result, int count) {
 			case 0x456: value = 0xB3; break;
 			case 0x457: value = 0xBF; break;
 			}
-			if(pb < pe)
-				*pb++ = (unsigned char)value;
-			break;
-		case CPU16LE:
-			if(pb < pe)
-				*pb++ = (unsigned char)(value & 0xFF);
-			if(pb < pe)
-				*pb++ = (unsigned char)(((unsigned)value >> 8));
-			break;
-		case CPU16BE:
-			if(pb < pe)
-				*pb++ = (unsigned char)(((unsigned)value >> 8));
-			if(pb < pe)
-				*pb++ = (unsigned char)(value & 0xFF);
-			break;
-		default:
-			if(pb < pe)
-				*pb++ = (unsigned char)value;
-			break;
+			cashed[cashed_count++] = (unsigned char)value;
+			size--;
 		}
 	}
-	return pb - (char*)result;
+	return count <= cashed_count;
 }
 
-unsigned text::getu() {
-	unsigned result = get();
-	autodetect(result);
-	switch(cp_src) {
-	case CPUTF8:
-		if(result >= 192 && result <= 223)
-			result = (result - 192) * 64 + (get() - 128);
-		else if(result >= 224 && result <= 239) {
-			auto p0 = get();
-			auto p1 = get();
-			result = (result - 224) * 4096 + (p0 - 128) * 64 + (p1 - 128);
+void text::next(unsigned count) {
+	while(true) {
+		if(makecashe(count)) {
+			cashed_pos += count;
+			return;
 		}
-		return result;
-	case CPU16LE:
-		result |= get() << 8;
-		return result;
-	case CP1251:
-		if(((unsigned char)result >= 0xC0))
-			return result - 0xC0 + 0x410;
-		else switch(result) {
-		case 0xB2: return 0x406;
-		case 0xAF: return 0x407;
-		case 0xB3: return 0x456;
-		case 0xBF: return 0x457;
-		}
-		return result;
-	default:
-		return result;
+		count -= cashed_count;
 	}
+}
+
+int	text::read(void* result, int count) {
+	auto r = 0;
+	auto c = imin(cashed_count - cashed_pos, (unsigned)count);
+	if(c) {
+		memcpy(result, cashed + cashed_pos, c);
+		cashed_pos += c;
+		r += c;
+	}
+	count -= c;
+	if(count)
+		r += parent.read((char*)result + c, count);
+	return r;
+}
+
+int	text::write(const void* result, int count) {
+	return parent.write(result, count);
+}
+
+int text::seek(int count, int rel) {
+	cashed_count = 0;
+	cashed_pos = 0;
+	return parent.seek(count, rel);
+}
+
+bool text::oneof(const char* value) {
+	if(!makecashe(1))
+		return false;
+	if(zchr(value, cashed[cashed_pos]))
+		return true;
+	return false;
+}
+
+bool text::skipws() {
+	if(oneof(" \t")) {
+		next(1);
+		return true;
+	}
+	return false;
+}
+
+bool text::ispair(const char* pairs) {
+	if(!makecashe(1))
+		return false;
+	auto sym = (unsigned char)cashed[cashed_pos];
+	for(int i = 0; pairs[i]; i += 2) {
+		if(sym >= (unsigned char)pairs[i] && sym <= (unsigned char)pairs[i + 1])
+			return true;
+	}
+	return false;
+}
+
+bool text::skipcr() {
+	if(is('\n')) {
+		if(is('\r'))
+			next(2);
+		else
+			next(1);
+		return true;
+	} else if('\r') {
+		if(is('\n'))
+			next(2);
+		else
+			next(1);
+		return true;
+	}
+	return false;
+}
+
+bool text::left(const char* value) {
+	unsigned count = zlen(value);
+	if(!makecashe(count))
+		return false;
+	if(memcmp(cashed + cashed_pos, value, count) != 0)
+		return false;
+	cashed_pos += count;
+	return true;
+}
+
+bool text::identifier(char* result, unsigned max_count) {
+	if(!is("AZazÀßàÿ", makecashe(1))
+		return false;
+	auto pb = result;
+	auto pe = result + max_count;
+	while((cashed[cashed_pos]>='A' && cashed[cashed_pos] <= 'Z')
+		|| (cashed[cashed_pos] >= 'a' && cashed[cashed_pos] <= 'z')
+		|| (cashed[cashed_pos] >= '0' && cashed[cashed_pos] <= '9')
+		|| (cashed[cashed_pos] >= 'À' && cashed[cashed_pos] <= 'ß')
+		|| cashed[cashed_pos] == '_') {
+		if(pb < pe)
+			*pb++ = cashed[cashed_pos];
+		cashed_pos++;
+		makecashe(1);
+	}
+	*pb = 0;
+	return true;
+}
+
+bool text::is(const char symbol) {
+	if(!makecashe(1))
+		return false;
+	return cashed[cashed_pos] == symbol;
+}
+
+bool text::is(const char symbol, unsigned index) {
+	if(!makecashe(index + 1))
+		return false;
+	return cashed[cashed_pos + index] == symbol;
+}
+
+unsigned char text::get() {
+	unsigned char sym;
+	if(!parent.read(&sym, 1))
+		return 0;
+	return sym;
 }
 
 void text::putu(unsigned value) {
-	switch(cp_dst) {
+	switch(cp) {
 	case CPUTF8:
 		if(((unsigned short)value) < 128)
 			put((unsigned char)value);
@@ -158,5 +211,35 @@ void text::putu(unsigned value) {
 	default:
 		put((unsigned char)value);
 		break;
+	}
+}
+
+unsigned text::getu() {
+	unsigned result = get();
+	switch(cp) {
+	case CPUTF8:
+		if(result >= 192 && result <= 223)
+			result = (result - 192) * 64 + (get() - 128);
+		else if(result >= 224 && result <= 239) {
+			auto p0 = get();
+			auto p1 = get();
+			result = (result - 224) * 4096 + (p0 - 128) * 64 + (p1 - 128);
+		}
+		return result;
+	case CPU16LE:
+		result |= get() << 8;
+		return result;
+	case CP1251:
+		if(((unsigned char)result >= 0xC0))
+			return result - 0xC0 + 0x410;
+		else switch(result) {
+		case 0xB2: return 0x406;
+		case 0xAF: return 0x407;
+		case 0xB3: return 0x456;
+		case 0xBF: return 0x457;
+		}
+		return result;
+	default:
+		return result;
 	}
 }
--- io_text.h
+++ io_text.h
@@ -4,41 +4,34 @@
 #pragma once
 
 namespace io {
-	struct text : stream {
-		int					read(void* result, int count) override;
-		int					seek(int count, int rel = SeekCur) override;
-		int					write(const void* result, int count) override;
-		text(stream& parent, codepages cp_src = CPNONE, codepages cp_dst = CP1251);
-	private:
-		codepages			cp_src;
-		codepages			cp_dst;
-		stream&				parent;
-		void				autodetect(unsigned& result);
-		unsigned char		get();
-		unsigned			getu();
-		void				put(unsigned char sym) { write(&sym, sizeof(sym)); }
-		void				putu(unsigned sym);
-	};
-	struct sequence : public stream {
-		unsigned char		get();
+	struct text : public stream {
+		bool				identifier(char* result, unsigned max_count);
+		bool				is(const char symbol);
+		bool				is(const char symbol, unsigned index);
+		bool				ispair(const char* pairs);
 		bool				left(const char* value);
+		void				next(unsigned count);
+		bool				oneof(const char* value);
 		int					read(void* result, int count) override;
 		int					seek(int count, int rel = SeekCur) override;
+		bool				skipcr(); // Skip one \n or \r or any pair of this.  Return true if one of this case have place.
+		bool				skipws(); // Skip all spaces or tabs. Return true if one of this case have place.
 		int					write(const void* result, int count) override;
-		sequence(io::stream& parent, codepages cp_src = CPNONE);
+		text(io::stream& parent, codepages cp = CPNONE);
 		operator bool() const { return false; }
 	private:
-		codepages			cp_src;
-		codepages			cp_dst;
+		codepages			cp;
 		char				cashed[64];
 		unsigned			cashed_pos;
 		unsigned			cashed_count;
 		io::stream&			parent;
 		//
-		unsigned char		getb();
+		unsigned char		get();
 		unsigned			getcount() const { return cashed_count - cashed_pos; }
 		unsigned			getu();
 		bool				makecashe(unsigned count);
+		void				put(unsigned char sym) { write(&sym, sizeof(sym)); }
+		void				putu(unsigned value);
 		void				shift();
 	};
 }
