682e17c7610bfa61a598ede539fef11cc4c52b28
pavelius <p000000000001@gmail.com>
2018-02-06 14:23:11 +0200

    Header form

--- widget_database.cpp
+++ widget_database.cpp
@@ -2,6 +2,7 @@
 #include "crt.h"
 #include "collections.h"
 #include "database.h"
+#include "draw.h"
 #include "draw_tree.h"
 #include "settings.h"
 #include "bsreq.h"
@@ -10,11 +11,39 @@ using namespace	draw;
 using namespace	draw::controls;
 using namespace database;
 
+static bool change_object(database::object& source) {
+	window dc(-1, -1, 400, 300, WFMinmax | WFResize, 0, "HeaderForm");
+	setcaption(source.name);
+	const int dy = texth() + 8 + metrics::padding * 2;
+	int current_control = 0;
+	setfocus(0, true);
+	while(true) {
+		rect rc = {0, 0, getwidth(), getheight() - dy};
+		rectf({0, 0, getwidth(), getheight()}, colors::form);
+		rc.offset(metrics::padding * 2);
+		rc.y1 = getheight() - dy - metrics::padding;
+		rc.x2 = getwidth() - metrics::padding;
+		rc.x1 = rc.x2 - 100; button(rc.x1, rc.y1, 100, KeyEscape, 0, "Отменить");
+		rc.x1 = rc.x1 - 100; button(rc.x1, rc.y1, 100, KeyEnter, 0, "OK");
+		int id = input();
+		switch(id) {
+		case 0:
+		case KeyEscape:
+			return false;
+		case KeyEnter:
+			return true;
+		default:
+			dodialog(id);
+			break;
+		}
+	}
+}
+
 static struct widget_database_header : tree {
 
 	void initialize() {
 		addcol(WidgetImage, "image", "Изображение");
-		addcol(WidgetField|ColumnSizeAuto|ColumnSmallHilite, "name", "Наименование");
+		addcol(WidgetField | ColumnSizeAuto | ColumnSmallHilite, "name", "Наименование");
 		fields = database::object_type;
 	}
 
@@ -39,7 +68,7 @@ static struct widget_database_header : tree {
 		for(auto& e : database::objects) {
 			if(e.parent == &row) {
 				auto hr = hasrows(e);
-				tree::addrow((unsigned)&e, hr ? TIGroup :0, hr ? 1 : 0, 0);
+				tree::addrow((unsigned)&e, hr ? TIGroup : 0, hr ? 1 : 0, 0);
 			}
 		}
 	}
@@ -51,6 +80,10 @@ static struct widget_database_header : tree {
 			auto pe = (element*)rows.get(index);
 			addrows(*((database::object*)pe->param));
 		}
+	}
+
+	bool changing(void* object, const char* id, unsigned flags) override {
+		return change_object(*((struct object*)object));
 	}
 
 	widget_database_header() {
